04ce212366f9d4942b5e44af6ffea57e8ab8bbea
vietdan1613 <viet.dan.1613@gmail.com>
2026-05-17 09:12:30 +0700

    problem2 feat: initialize vite react typescript project

---
+++ src/problem2/currency-swap-form/src/App.tsx
@@ -0,0 +1,9 @@
+import { Toaster } from 'react-hot-toast';
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-4">
+      <Toaster position="top-right" />
+    </main>
+  );
+}
